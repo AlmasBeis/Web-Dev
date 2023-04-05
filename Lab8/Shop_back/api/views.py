597ef7d842ea6a62358dd49c4c35a5a4cc0c79df
Almas <almas.beysebaev@gmail.com>
2023-04-05 20:26:30 +0600

    Lab 8

---
+++ Lab8/Shop_back/api/views.py
@@ -0,0 +1,46 @@
+from django.http import HttpResponse
+
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Welcome to the shop")
+
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductByID(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryByID(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryCreate(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductByCategory(APIView):
+    def get(self, request, category_id):
+        category = Category.objects.get(id=category_id)
+        products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
