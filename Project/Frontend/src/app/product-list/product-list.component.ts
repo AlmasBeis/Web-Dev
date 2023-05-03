2aa1c571ceb474cd748103643d8f9eeb27d54c8d
Алмас Бейсебаев <a_beisebaev@kbtu.kz>
2023-05-04 03:03:51 +0600

    Project

--- Project/Frontend/src/app/product-list/product-list.component.ts
+++ Project/Frontend/src/app/product-list/product-list.component.ts
@@ -4,6 +4,8 @@ import {ProductService} from "../product.service";
 import {UserService} from "../user.service";
 import {CartService} from "../cart.service";
 import {CartItem} from "../interfaces/cart-item";
+import {User} from "../interfaces/user";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-product-list',
@@ -14,9 +16,26 @@ export class ProductListComponent implements OnInit{
   products: Product[]=[];
   cart: CartItem[]=[];
 
+  category_id: number = 0;
+  name: string = '';
+  price:number = 0;
+  image: string = '';
+  description: string = '';
+
+
+
+
+
   constructor(private productService: ProductService, private userService: UserService, private cartService: CartService) {
   }
   logged = this.userService.logged
+
+  is_super: Boolean = false;
+
+  user: Observable<User> = this.userService.getUser();
+
+  addProductForm: Boolean = false;
+
   ngOnInit(): void {
     this.productService.getProducts().subscribe(product => {
       this.products = product
@@ -25,6 +44,9 @@ export class ProductListComponent implements OnInit{
           this.cart=cart;
         }
     );
+    this.user.subscribe(data=>{
+      this.is_super = data.is_superuser
+    })
   }
   // addToCart(){
   //   this.cartService.createCart(this.product)
@@ -33,7 +55,20 @@ export class ProductListComponent implements OnInit{
   addToCart(id: number, event: Event) {
     this.cartService.addToCart(id,this.cart).subscribe(product=>{})
     event.stopPropagation();
+
     alert("go to cart")
   }
+  // addProduct(category_id: number, name:string, price: number, description: string, image: string){
+  //   this.productService.createProduct(category_id,name,price,description,image).subscribe(data=>{})
+  // }
 
+  onSubmit() {
+    this.productService.createProduct(this.category_id, this.name, this.price, this.description, this.image).subscribe(
+        data=> {alert("product created"); window.location.reload();
+        },error => {
+      console.error(error);
+      alert("wrong data ");
+    })
+
+  }
 }
